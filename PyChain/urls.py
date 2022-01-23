61a094a35d063a178993bf7e0c9782ac6412e27a
paul <paulbrowne2@gmail.com>
2022-01-23 07:43:42 +0000

    Created cryptocurrency, add transactions to our block, allow other nodes to connect to network

--- PyChain/urls.py
+++ PyChain/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from blockchain import views
+from blockchain.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('^get_chain$', views.get_chain, name="get_chain"),
+    re_path('^mine_block$', views.mine_block, name="mine_block"),
+    re_path('^is_valid$', views.is_valid, name="is_valid"),
+    re_path('^add_transaction$', views.add_transaction, name="add_transaction"),
+    re_path('^is_valid$', views.is_valid, name="is_valid"),
+    re_path('^connect_node$', views.connect_node, name="connect_node"),
+    re_path('^replace_chain$', views.replace_chain, name="replace_chain"),
 ]
